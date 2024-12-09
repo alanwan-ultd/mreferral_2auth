3fac6f85f27af4d203bfb7387d904df562789df7
AlanWan <alanwan00@yahoo.com.hk>
2024-12-09 11:32:51 +0800

    remove listing buttons

--- 2fa/admin/inc/views/list-footer.php
+++ 2fa/admin/inc/views/list-footer.php
@@ -1,12 +1,13 @@
-<?php if(intval($sectionPermission[1]) == 1){  //check read permission ?>
+<?php $hideButtons = true; ?>
+<?php if(!$hideButtons && intval($sectionPermission[1]) == 1){  //check read permission ?>
             <div class="footer-control">Bulk action
                 <a class="btn btn-sm btn-success" href="javascript:statusUpdate('A')"><i class="icon-check icons"></i>&nbsp; Active</a>
                 <a class="btn btn-sm btn-warning mr-1 normal" href="javascript:statusUpdate('P')"><i class="icon-close icons"></i>&nbsp; Pending</a>
-<?php if(intval($sectionPermission[2]) == 1){  //publish ?>
+<?php if(!$hideButtons && intval($sectionPermission[2]) == 1){  //publish ?>
                 <a class="btn btn-sm btn-info mr-1" href="javascript:publishUpdate()"><i class="icon-cloud-upload icons"></i>&nbsp; Publish</a>
 <?php } ?>
 
-<?php if(intval($sectionPermission[2]) != 1 ){  //publish ?>
+<?php if(!$hideButtons && intval($sectionPermission[2]) != 1 ){  //publish ?>
                 <a class="btn btn-sm btn-info mr-1" href="javascript:publishUpdate(true)"><i class="icon-cloud-upload icons"></i>&nbsp; Submit for Approval</a>
 <?php } ?>
 
